c9cb20046e044c8943c71ef961db31f50a2346ff
郭建泉 <jasonguojq@163.com>
2026-07-28 22:03:41 +0800

    初始化提交

---
+++ src/main/java/com/gjq/railwayticketingsystem/RailwayTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gjq.railwayticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayTicketingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gjq/railwayticketingsystem/RailwayTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjq.railwayticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
